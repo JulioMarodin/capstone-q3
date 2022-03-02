f0546a890bcdb02b5c0d9b27a26c1beb17de21ad
Victor Varela <vitaotapioqueiro@gmail.com>
2022-03-01 22:21:27 -0300

    initial commit6

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from app.configs import database, migrations
 from app import routes
+import os
 
 
 
@@ -8,6 +9,9 @@ def create_app():
     app = Flask(__name__)
 
     app.config['JSON_SORT_KEYS'] = False
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
     database.init_app(app)
     migrations.init_app(app)

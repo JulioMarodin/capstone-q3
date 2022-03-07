2af52bceff324ad3f57f92de345e5048de98e4d8
Tulio <tulio.gp2@gmail.com>
2022-03-07 15:17:11 +0100

    fix: relationship cars => category_models and fix variables names

--- app/models/category_car_model.py
+++ app/models/category_car_model.py
@@ -1,25 +1,28 @@
 from dataclasses import dataclass
 from sqlalchemy import Column, Float, String, Integer
 from app.configs.database import db
+from sqlalchemy.orm import relationship, backref
 
 @dataclass
-class Categorie_car(db.Model):
-    keys = ["body_types", "fuel_type", "engine_power", "km_per_liter", "allowed_categorie_cnh", "differentials"]
+class Category_car(db.Model):
+    keys = ["body_types", "fuel_type", "engine_power", "km_per_liter", "allowed_category_cnh", "differentials"]
 
-    categorie_id: int
+    category_id: int
     body_types: str
     fuel_type: str
     engine_power: str
     km_per_liter: float
-    allowed_categorie_cnh: str
+    allowed_category_cnh: str
     differentials: str
 
-    __tablename__ = 'tb_categorie_car'
+    __tablename__ = 'tb_category_car'
 
-    categorie_id = Column(Integer, primary_key=True)
+    category_id = Column(Integer, primary_key=True)
     body_types = Column(String, nullable=False)
     fuel_type = Column(String, nullable=False)
     engine_power = Column(String, nullable=False)
     km_per_liter = Column(Float, nullable=False)
-    allowed_categorie_cnh = Column(String(1), nullable=False)
+    allowed_category_cnh = Column(String(1), nullable=False)
     differentials = Column(String, nullable=False)
+
+    cars = relationship("Cars", backref=backref("category", uselist=False))

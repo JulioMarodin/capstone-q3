c91911c7b87fe00a6db5a76bbb1e926c860c1fcf
Anderson Silva <klaito.b@gmail.com>
2022-03-08 09:25:23 -0300

    feat: add get address by states

--- app/routes/address_routes.py
+++ app/routes/address_routes.py
@@ -5,3 +5,4 @@ from app.controllers import address_controller
 bp = Blueprint("address", __name__, url_prefix="/address")
 
 bp.post("")(address_controller.create_address)
+bp.get("")(address_controller.get_states)
